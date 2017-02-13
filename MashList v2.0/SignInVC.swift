088333a9774884e434183d7525f783fffcf89fcf
Nigel Karan <Nigel@Nigels-iMac.local>
2017-02-13 15:00:02 +0100

    SignIn UI Complete

---
+++ MashList v2.0/SignInVC.swift
@@ -0,0 +1,22 @@
+//
+//  SignInVC.swift
+//  MashList v2.0
+//
+//  Created by Nigel Karan on 13/02/17.
+//  Copyright © 2017 MashBin. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+   
+}
